3139dbb512757c03378a550418c0610bb566442a
Kimberly <kira0001@stud.kea.dk>
2025-09-28 11:15:15 +0200

    mostly done

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+export default function typeWriter(el, message, speed = 50) {
+  let i = 0;
+  function type() {
+    if (i < message.length) {
+      el.textContent += message.charAt(i);
+      i++;
+      setTimeout(type, speed);
+    }
+  }
+  type();
+}
